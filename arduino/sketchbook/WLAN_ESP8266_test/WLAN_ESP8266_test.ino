1d972d19242ab2c2474f9ad744d92726e1aad7a5
Bernhard "Ted" Fröhlich <ted@convey.de>
2017-09-05 21:51:33 +0200

    ...

--- arduino/sketchbook/WLAN_ESP8266_test/WLAN_ESP8266_test.ino
+++ arduino/sketchbook/WLAN_ESP8266_test/WLAN_ESP8266_test.ino
@@ -14,28 +14,49 @@
 
 #include <SoftwareSerial.h>
 
-#define INBUF_SIZE 100
+#define INBUF_SIZE 500
+#define LINEBUF_SIZE 100
+#define OUTBUF_SIZE 100
+
+void LogByte(char c);
+void LogString(const char *str);
 
 SoftwareSerial mySerial = SoftwareSerial(3, 4);
 
+/* Global Status */
 enum {
   Reset,
   Init,
   Idle,
-  DataWait
+  Processing, // Processing received data, do not call processing loop again
+  DataWait // Waiting for specific data
 } Status;
+
+/* Status of all TCP connections */
+enum {
+  ConnClosed,
+  ConnIdle
+} ConnStatus[5];
+
 long LastStatusChange;
 
 int cmd_i = 0;
-char Buf[20];
+char Buf[20]; // Buffer for commands incoming from serial line
 int out_i = 0;
 int in_w = 0;
 int in_r = 0;
-char InBuf[INBUF_SIZE];
+int out_w = 0;
+int out_r = 0;
+char InBuf[INBUF_SIZE]; // Buffer for incoming data from WLAN modile
+char LineBuf[LINEBUF_SIZE]; // Buffer for one \r terminated line of incoming data from WLAN module
+char OutBuf[OUTBUF_SIZE]; // Buffer for debugging data to be sent via serial line when idle.
+                          // Sending data via serial line while WLAN data is pending may lead to lost data....
 unsigned long LastDataTime = 0;
 
 
 void setup() {
+  int i;
+  
 //  mySerial.begin(9600);
 //  mySerial.begin(19200); // Maximum for 8 MHz clock?
 //  mySerial.begin(38400);
@@ -53,6 +74,10 @@ void setup() {
   LastStatusChange = millis();
   Serial.println("AT+RST");
   LastDataTime = millis();
+
+  for(i = 0; i < 5; i++) {
+    ConnStatus[i] = ConnIdle;
+  }
 }
 
 void HandleCommand(char *Buf) {
@@ -74,8 +99,14 @@ void HandleCommand(char *Buf) {
   } else if (strcmp(Buf, "0") == 0) {
     Serial.println("AT");
     LastDataTime = millis();
-  } else if (strcmp(Buf, "?") == 0) {    
+  } else if (strcmp(Buf, "m?") == 0) {    
     Serial.println("AT+CIPMUX?");
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "m0") == 0) {    
+    Serial.println("AT+CIPMUX=0");
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "m1") == 0) {    
+    Serial.println("AT+CIPMUX=1");
     LastDataTime = millis();
   } else if (strcmp(Buf, "1") == 0) {    
     Serial.println("AT+CIPSTART=\"TCP\",\"192.168.178.41\",80,60");
@@ -89,26 +120,59 @@ void HandleCommand(char *Buf) {
   } else if (strcmp(Buf, "9") == 0) {    
     Serial.println("AT+CIPCLOSE");
     LastDataTime = millis();
+
+  /* Soft-AP Tests */
+  } else if (strcmp(Buf, "s?") == 0) {    
+    Serial.println("AT+CWMODE_CUR?");
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "s0") == 0) {    
+    Serial.println("AT+CWMODE_CUR=3");
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "s1") == 0) {    
+    Serial.println("AT+CWSAP_CUR=\"ORKUS\",\"12345678\",5,0");
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "s2") == 0) {    
+    Serial.println("AT+CIPSERVER=1,80");
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "s3") == 0) {
+    Serial.println("AT+CIPSEND=0,106");
+  } else if (strcmp(Buf, "s4") == 0) {
+    const char *Result="<html><head><title>TEST ESP8266</title></head><body><h1>Test ESP8266</h1><p>Dummy Text</p></body></html>";    
+    Serial.println(Result);
+    LastDataTime = millis();
+  } else if (strcmp(Buf, "s9") == 0) {
+    Serial.println("AT+CIPCLOSE=0");
   } else {
     mySerial.println("Unknown WLAN command");
     mySerial.println(Buf);
   }
 }
 
-void WLANListenLoop() {
-  int c;
-  long utics;
+/*
+ * Call with Wait_usec 500 to 1000 if you expect a bigger data block which you want to receive 
+ * as a whole. Call with 0 if you just want to clear the receiver queue.
+ */
+void WLANListenLoop(unsigned long Wait_usec) {
+  unsigned long utics;
+  char c;
 
   utics = micros();
   do {
     while (Serial.available()) {
-      InBuf[in_w++] = Serial.read();
+      c = Serial.read();
+      InBuf[in_w++] = c;
       if (in_w >= INBUF_SIZE) in_w = 0;
+      if (c == '\r' && Status != Processing) {
+        Status = Processing;
+        WLANCheckData();
+        Status = DataWait;
+      }
       utics=micros();
       LastDataTime = millis();
     }
     if (utics > micros()) utics=micros();
-  } while(micros()-utics < 500);
+  } while(micros()-utics < Wait_usec);
+  if (Wait_usec > 100) Status = Idle;
 }
 
 void CmdListenLoop() {
@@ -131,16 +195,76 @@ void CmdListenLoop() {
   }
 }
 
-void WLANCheckData() {
+void HandleLine() {
+  LogString(LineBuf);
+  LogString("|\r\n");
+}
+
+bool WLANCheckData() {  
+  int i = 0;
+  bool LineComplete = false;
+  int Save_in_r = in_r;
+  
   while(in_w != in_r) {
-    mySerial.write(InBuf[in_r]);
+    LogByte(InBuf[in_r]);
+    if (InBuf[in_r] == '\n') {
+      // Do nothing, just ignore \n
+    } else if (InBuf[in_r] == '\r') {
+      // End of command line, break loop
+      in_r++;
+      if (in_r >= INBUF_SIZE) in_r = 0;
+      LineBuf[i] = '\0';
+      LineComplete = true;
+      break;
+    } else {
+      LineBuf[i++] = InBuf[in_r];
+    }
+    
     in_r++;
     if (in_r >= INBUF_SIZE) in_r = 0;
+
+    if (i >= LINEBUF_SIZE-1) {
+      // Ugh... Buffer overflow. This is not good... treat as line break?
+      LineBuf[i] = '\0';
+      LineComplete = true;
+      break;
+    }
+  }
+
+  if (LineComplete) {
+    WLANListenLoop(0); // Just store pending WLAN data in InBuf
+    HandleLine();
+  }
+
+  return (LineComplete);
+}
+
+void LogByte(char c) {
+  OutBuf[out_w++] = c;
+  if (out_w >= OUTBUF_SIZE) out_w = 0;
+  if (out_w == out_r) {
+    // Buffer overflow...
+    out_r++;
+    if (out_r >= OUTBUF_SIZE) out_r = 0;
   }
 }
 
+void LogString(const char *str) {
+  while(*str != '\0') {
+    LogByte(*str);
+    str++;
+  }
+}
+
+
+void SendDebugData() {
+  while(out_w != out_r) {
+    mySerial.write(OutBuf[out_r++]);
+    if (out_r >= OUTBUF_SIZE) out_r = 0;
+  }
+}
 void loop() {
-  WLANListenLoop();
-  WLANCheckData();
+  WLANListenLoop(500);
+  SendDebugData();
   CmdListenLoop();
 }
