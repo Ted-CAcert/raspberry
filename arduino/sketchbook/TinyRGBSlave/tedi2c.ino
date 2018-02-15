081df7ccbcfcab64d366a85b089f5e99ffe3ffdf
Bernhard Fröhlich <bernhard@cacert.org>
2018-02-15 23:18:03 +0100

    Zwischenstand

--- arduino/sketchbook/TinyRGBSlave/tedi2c.ino
+++ arduino/sketchbook/TinyRGBSlave/tedi2c.ino
@@ -16,8 +16,17 @@ static volatile uint8_t TWI_RxTail;
 static volatile uint8_t TWI_curByte;
 static volatile uint8_t TWI_curBit;
 
+static volatile uint8_t *pInRegister;
+static volatile uint8_t *pOutRegister;
+static volatile uint8_t *pModeRegister;
+static uint8_t bitSDA;
+static uint8_t bitSCL;
+
+
 #define TWISTATUS_STOP 0
 #define TWISTATUS_START 1
+#define TWISTATUS_SEND 2
+#define TWISTATUS_REQUEST 3
 static volatile uint8_t TWI_Status;
 
 // This function is not part of the library for ATTiny...
@@ -38,12 +47,91 @@ void Flush_TWI_Buffers(void)
   TWI_RxHead = 0;
 }
 
+// Check if there is data in the receive buffer.
+unsigned char USI_TWI_Data_In_Receive_Buffer(void)
+{
+  unsigned char tmpRxTail;          // Temporary variable to store volatile
+  tmpRxTail = TWI_RxTail;           // Not necessary, but prevents warnings
+  return (TWI_RxHead - tmpRxTail) & TWI_RX_BUFFER_MASK; // Return 0 (FALSE) if the receive buffer is empty.
+}
+
+uint8_t TWI_ReadByte() {
+  // ToDo...
+  TWI_Status = TWISTATUS_STOP;
+  return 0;  
+}
+
+void TWI_SendACK() {
+  // ToDo...
+}
+
+void TWI_SendNACK() {
+  // ToDo...
+}
+
+
+/* This is called on a CHANGE event of PIN_SDA.
+   If SDA changes while SCL is HIGH this is either a START event 
+   (if SDA goes LOW) or a STOP event (if SDA goes HIGH) of the 
+   I2C bus protocol.
+*/
 void TWI_ISR()
 {
-  
+  // Check current status of SCL
+  if (*pInRegister & bitSCL) {
+    if (*pInRegister & bitSDA) {
+      // This is a STOP condition...
+      TWI_Status = TWISTATUS_STOP;
+    } else {
+      // Hmm, what if there was already a START without STOP? Currently ignoring that...
+      uint8_t CurByte;
+      
+      TWI_Status = TWISTATUS_START;
+      CurByte = TWI_ReadByte();
+      // There might have been a STOP during receiving...
+      if ((TWI_Status = TWISTATUS_START) && (CurByte == 0 || CurByte>>1 == TWI_slaveAddress)) {
+        unsigned char tmpRxHead;
+        unsigned char tmpTxTail; // Temporary variables to store volatiles
+        
+        // CurByte == 0 is broadcast...
+        if (CurByte & 1) {
+          TWI_Status = TWISTATUS_SEND;
+          // Ignoring this at the moment...
+        } else {
+          TWI_Status = TWISTATUS_REQUEST;
+          TWI_SendACK();
+          while(TWI_Status == TWISTATUS_SEND) {
+            CurByte = TWI_ReadByte();
+            
+            tmpRxHead = (TWI_RxHead + 1) & TWI_RX_BUFFER_MASK;
+            if (TWI_RxTail != tmpRxHead) {
+              TWI_RxHead            = tmpRxHead;
+              TWI_RxBuf[TWI_RxHead] = CurByte;
+              TWI_SendACK();
+            } else // If the buffer is full then:
+            {
+              TWI_SendNACK();
+              break;
+            }
+          }
+          if (TWI_RxHead != TWI_RxTail && USI_TWI_On_Slave_Receive) {
+            USI_TWI_On_Slave_Receive(USI_TWI_Data_In_Receive_Buffer());
+          }
+        }
+      }
+    }
+  }
 }
 
 TedWire::TedWire() {
+  // This is assuming we are on ATTiny, where we have only pone port B
+  // so I'm saving the second set of register pointers
+  // Using pointers to (hopefully) save cycles...
+  pInRegister = portInputRegister(digitalPinToPort(PIN_SDA));
+  pOutRegister = portOutputRegister(digitalPinToPort(PIN_SDA));
+  pModeRegister = portModeRegister(digitalPinToPort(PIN_SDA));
+  bitSDA = digitalPinToBitMask(PIN_SDA);
+  bitSCL = digitalPinToBitMask(PIN_SCL);  
 }
 
 void TedWire::begin(uint8_t address) {
@@ -52,7 +140,15 @@ void TedWire::begin(uint8_t address) {
 
   TWI_slaveAddress = address;
 
-  USI_TWI_On_Slave_Receive = 0;
+  // Set both pins to INPUT, adapted from wiring_digital.c
+  { 
+    uint8_t oldSREG = SREG;
+    cli();
+    *pModeRegister &= ~(bitSDA | bitSCL);
+    *pOutRegister &= ~(bitSDA | bitSCL);
+    SREG = oldSREG;
+  }  
+
   attachInterrupt(digitalPinToInterrupt(PIN_SDA), TWI_ISR, CHANGE);
 }
 
