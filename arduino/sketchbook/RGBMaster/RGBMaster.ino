dc2361d30bda3f6ae9df3f7225cfd7e0f997875a
Bernhard Fröhlich <bernhard@cacert.org>
2018-03-04 16:52:49 +0100

    RGBMaster, test implementation only...

--- arduino/sketchbook/RGBMaster/RGBMaster.ino
+++ arduino/sketchbook/RGBMaster/RGBMaster.ino
@@ -2,18 +2,31 @@
 
 void setup() {
   Wire.begin(); // join i2c bus (address optional for master)
+
+  Wire.setClock(31000); // Max. frequency for 8MHz slave is 50 kHz, but 31 kHz leaves a bit reserve
+/*    
+  // Set the deviceID by writing new ID in address 3
+  Wire.beginTransmission(8); // transmit to device #8 (0 is broadcast)
+  Wire.write(3);        // Write to address 3, Device ID
+  Wire.write(8);         
+  Wire.endTransmission();    // stop transmitting
+*/  
 }
 
-byte x = 0;
+byte x = 50;
 
 void loop() {
+  Wire.setClock(31000); // Max. frequency for 8MHz slave is 50 kHz, but 31 kHz leaves a bit reserve
+    
   Wire.beginTransmission(8); // transmit to device #8
   Wire.write(0);        // Write to address 0
   Wire.write(x);         
-  Wire.write(255-x);         
+  Wire.write(x);         
+  Wire.write(x);         
   Wire.endTransmission();    // stop transmitting
 
-  x += 5;
-  if (x > 255) x = 0;
+  if (x == 50) x = 200;
+  else x = 50;
+
   delay(500);
 }
