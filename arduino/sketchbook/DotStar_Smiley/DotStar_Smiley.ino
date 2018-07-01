4cdba52819db413bcd5e4e9e7c7d273cf00b5109
Bernhard "Ted" Fröhlich <ted@convey.de>
2018-07-01 14:21:22 +0200

    Dotstar Smiley

---
+++ arduino/sketchbook/DotStar_Smiley/DotStar_Smiley.ino
@@ -0,0 +1,110 @@
+// Simple strand test for Adafruit Dot Star RGB LED strip.
+// This is a basic diagnostic tool, NOT a graphics demo...helps confirm
+// correct wiring and tests each pixel's ability to display red, green
+// and blue and to forward data down the line.  By limiting the number
+// and color of LEDs, it's reasonably safe to power a couple meters off
+// the Arduino's 5V pin.  DON'T try that with other code!
+
+#include <Adafruit_DotStar.h>
+// Because conditional #includes don't work w/Arduino sketches...
+#include <SPI.h>         // COMMENT OUT THIS LINE FOR GEMMA OR TRINKET
+//#include <avr/power.h> // ENABLE THIS LINE FOR GEMMA OR TRINKET
+
+#define NUMPIXELS 64 // Number of LEDs in strip
+
+// Here's how to control the LEDs from any two pins:
+#define DATAPIN    5
+#define CLOCKPIN   4
+Adafruit_DotStar strip = Adafruit_DotStar(
+  NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+// The last parameter is optional -- this is the color data order of the
+// DotStar strip, which has changed over time in different production runs.
+// Your code just uses R,G,B colors, the library then reassigns as needed.
+// Default is DOTSTAR_BRG, so change this if you have an earlier strip.
+
+// Hardware SPI is a little faster, but must be wired to specific pins
+// (Arduino Uno = pin 11 for data, 13 for clock, other boards are different).
+//Adafruit_DotStar strip = Adafruit_DotStar(NUMPIXELS, DOTSTAR_BRG);
+
+int colindex=0;
+unsigned long color_list[] = {
+  0xFF0000,
+  0x00FF00,
+  0x0000FF,
+  0xFFFF00,
+  0xFF00FF,
+  0x00FFFF,
+  0x0
+};
+
+void setup() {
+
+#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000L)
+  clock_prescale_set(clock_div_1); // Enable 16 MHz on Trinket
+#endif
+
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+  strip.setBrightness(8);
+}
+
+// Runs 10 LEDs at a time along strip, cycling through red, green and blue.
+// This requires about 200 mA for all the 'on' pixels + 1 mA per 'off' pixel.
+
+int      head  = 0, tail = -10; // Index of first 'on' and 'off' pixels
+uint32_t color = 0xFF0000;      // 'On' color (starts red)
+int frame=0;
+
+int8_t icon[][64] = {
+  {
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0, 1, 1, 0,
+    0, 1, 1, 0, 0, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 0, 0, 0, 0, 1, 0,
+    0, 0, 1, 1, 1, 1, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  },{
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0, 0, 0, 0,
+    0, 1, 1, 0, 0, 1, 1, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 1, 0,
+    0, 1, 0, 0, 0, 1, 0, 0,
+    0, 0, 1, 1, 1, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  }
+}
+;
+
+unsigned long frame_time[] = {
+  5000,
+  100
+};
+
+void loop() {
+  int row, col;
+
+  color = color_list[colindex];
+  for(row=0; row<8; row++) {
+    for(col=0; col<8; col++) {
+      if (icon[frame][row*8+col]) {
+        strip.setPixelColor(row*8+7-col, color); // 'On' pixel
+      } else {
+        strip.setPixelColor(row*8+7-col, 0); // 'Off' pixel
+      }
+    }
+  }
+  strip.show();                     // Refresh strip
+  delay(frame_time[frame]);                        // Pause milliseconds
+
+
+  frame++;
+  if (frame>1) {
+    frame=0;
+    colindex++;
+    if (color_list[colindex] == 0) colindex=0;
+  }
+}
+
